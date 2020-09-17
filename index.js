d5668fdc8302d9660a0c1d5aca3c2d61ee749eb7
denev <65989857+deneb0723@users.noreply.github.com>
2020-09-18 07:25:13 +0900

    middleware 설정, babel/node 설치, code 최신화

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListen = () =>
+  console.log(`Listening on : https://localhost:${PORT}`);
+
+app.use(cookieParser());
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", (req, res) => res.send("Home"));
+app.get("/profile", (req, res) => res.send("Profile"));
+
+app.listen(4000, handleListen);
