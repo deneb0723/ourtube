912335cd7d860c4fa0cb6f579266ff8b374222bb
denev <65989857+deneb0723@users.noreply.github.com>
2020-09-18 11:53:40 +0900

    routes, init 파일 생성

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {
+  userRouter
+} from "./routes";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListen = () =>
-  console.log(`Listening on : https://localhost:${PORT}`);
-
 app.use(cookieParser());
 app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(morgan("dev"));
 app.use(helmet());
 
 app.get("/", (req, res) => res.send("Home"));
+
 app.get("/profile", (req, res) => res.send("Profile"));
 
-app.listen(4000, handleListen);
+app.use("/user", userRouter);
+
+export default app
