6d3f07cb1afab91e8b3384c13b1709081e8e8875
denev <65989857+deneb0723@users.noreply.github.com>
2020-10-16 22:49:18 +0900

    실시간 댓글 기능 추가

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
@@ -166,3 +167,25 @@ export const postRegisterView = async (req, res) => {
     res.end();
   }
 }
+
+// Add comment
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
